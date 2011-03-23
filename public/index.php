ba359e649bd01ecccd70f5f4a544826c459264e1
J.D. Fitz.Gerald <jaydee@guttingfish.com>
2011-03-23 23:20:01 +0000

    ook

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@ echo "<br>cwd<br>";
 echo getcwd();
 
 
-print_R(stat('index.php'));
+print_R($x=stat('index.php'));
+
+print_r(posix_getpwuid($x['uid']));
 echo 'ook';
 ini_set('memory_limit','1000M');
 echo 'ook';
