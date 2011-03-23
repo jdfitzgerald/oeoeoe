2d4810c175a9894144eb9a22ea2dcdb585c413d4
J.D. Fitz.Gerald <jaydee@guttingfish.com>
2011-03-23 23:05:51 +0000

    index

--- public/index.php
+++ public/index.php
@@ -1,33 +1,25 @@
 <?
-echo "<br>ls<br>";
-echo `ls`;
 
 echo "<br>cwd<br>";
 echo getcwd();
 
-echo "<br>proc<br>";
-$descriptorspec = array( 0 => array("pipe", "r"), 1 => array("pipe", "w"));
+$dir    = '/tmp';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
 
-$cwd = '/tmp';
-$env = array('some_option' => 'aeiou');
+print_r($files1);
+print_r($files2);
 
-$process = proc_open('php', $descriptorspec, $pipes, $cwd, $env);
+$dir    = '/home';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
 
-if (is_resource($process)) {
-  // $pipes now looks like this:
-  // 0 => writeable handle connected to child stdin
-  // 1 => readable handle connected to child stdout
-  // Any error output will be appended to /tmp/error-output.txt
+print_r($files1);
+print_r($files2);
 
-  fwrite($pipes[0], '<?php print_r($_ENV); ?>');
-  fclose($pipes[0]);
+$dir    = '';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
 
-  echo stream_get_contents($pipes[1]);
-  fclose($pipes[1]);
-
-  // It is important that you close any pipes before calling
-  // proc_close in order to avoid a deadlock
-  $return_value = proc_close($process);
-
-  echo "command returned $return_value\n";
-}
+print_r($files1);
+print_r($files2);
