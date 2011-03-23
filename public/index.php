674d2335a0842ec7e293dac8765c728108fab2d9
J.D. Fitz.Gerald <jaydee@guttingfish.com>
2011-03-23 23:28:21 +0000

    ook

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?
-print_r($_SERVER);
+//print_r($_SERVER);
 echo "<br>cwd<br>";
 echo getcwd();
 
 
-print_R($x=stat('index.php'));
+//print_R($x=stat('index.php'));
 
 print_r(posix_getpwuid($x['uid']));
 echo 'ook';
@@ -13,6 +13,7 @@ echo "mem ".memory_get_usage()."<br>";
 $mult=1024;
 $x = str_repeat('x',$mult);
 $y = str_repeat($oneK,$mult);
+echo "mem ".memory_get_usage()."<br>";
 
 ini_set('memory_limit','1000M');
 $x = $y<1;
