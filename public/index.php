dd7c169880cb7cde7d934671076936ac8a270308
J.D. Fitz.Gerald <jaydee@guttingfish.com>
2011-03-23 23:06:53 +0000

    index

--- public/index.php
+++ public/index.php
@@ -3,23 +3,4 @@
 echo "<br>cwd<br>";
 echo getcwd();
 
-$dir    = '/tmp';
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
-
-print_r($files1);
-print_r($files2);
-
-$dir    = '/home';
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
-
-print_r($files1);
-print_r($files2);
-
-$dir    = '';
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
-
-print_r($files1);
-print_r($files2);
+print_r($_ENV);
